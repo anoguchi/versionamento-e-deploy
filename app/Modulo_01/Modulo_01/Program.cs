757a8e3ffcfe902f67a36f91fb2ca4ed5baac2e6
Alberto Noguchi <alberto2611@gmail.com>
2021-09-28 13:03:48 -0300

    first commit

---
+++ app/Modulo_01/Modulo_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Modulo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tamanhoArray = 10;
+            double[] numeros = new double[tamanhoArray];
+
+            for (int i = 0; i < tamanhoArray; i++)
+            {
+                Console.Write($"Digite número {i + 1}: ");
+                numeros[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Digite um número pra ver se ele existe");
+            int numeroInput = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tamanhoArray; i++)
+            {
+                if (numeroInput == numeros[i])
+                {
+                    Console.WriteLine($"Encontrado! Posição: {i + 1}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Não encontrado!");
+                    break;
+                }
+            }
+
+            if(numeroInput % 2 != 0)
+            {
+                Console.WriteLine("Número digitado é impar");
+            }
+            else
+            {
+                Console.WriteLine("Número digitado é par");
+            }
+        }
+    }
+}
